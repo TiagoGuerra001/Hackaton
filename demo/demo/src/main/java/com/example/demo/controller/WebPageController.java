b1af5bf39fffa402deaf23b3da1de65d05c4aa44
Bruno Braga <bf.braga@campus.fct.unl.pt>
2024-05-11 10:51:53 +0100

    fixed and improvements #11

--- demo/demo/src/main/java/com/example/demo/controller/WebPageController.java
+++ demo/demo/src/main/java/com/example/demo/controller/WebPageController.java
@@ -1,13 +1,27 @@
 package com.example.demo.controller;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.config.UserDataModel;
+import com.example.demo.entity.UserDAO;
 
 @Controller
 public class WebPageController {
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        // i want to get the authenticated user here using the securiry context
+        UserDataModel user = (UserDataModel) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDAO userDAO = new UserDAO();
+        userDAO.setUsername(user.getUsername());
+        userDAO.setEmail(user.getEmail());
+        userDAO.setPassword(user.getPassword());
+        model.addAttribute("userEnter", userDAO);
+
         return "home";
     }
 
@@ -25,7 +39,6 @@ public class WebPageController {
     public String getMethodName() {
         return "dashboard";
     }
-        
 
     @RequestMapping("/contact")
     public String contact() {
